a2edb8ea41eb9bef62857bf5aa11466388378b50
doktrjegih <doktrjegih@gmail.com>
2021-10-14 19:37:25 +0500

    расшариваю

--- main.py
+++ main.py
@@ -1,23 +1,41 @@
+'''
+пользак выбирает категорию из предложенных и вписывает руками сумму, дату, описание
+бот вписывает расход в нужное место пока хватит
+'''
+
 import telebot
 from telebot import types
 
-bot = telebot.TeleBot("")
+bot = telebot.TeleBot("2041146411:AAGW4K_Dnii5xb6Q9tnxSexk_9kFc_LofPQ")
 
 
-@bot.message_handler(commands=['start'])
-def eee(message):
-	chat_id = message.chat.id
-	markup = types.ReplyKeyboardMarkup()
-	itembtn_1 = types.KeyboardButton('категория1')
-	itembtn_2 = types.KeyboardButton('категория2')
-	itembtn_3 = types.KeyboardButton('категория3')
-	itembtn_4 = types.KeyboardButton('категория4')
-	itembtn_5 = types.KeyboardButton('категория5')
-	itembtn_6 = types.KeyboardButton('категория6')
-	markup.row(itembtn_1, itembtn_2)
-	markup.row(itembtn_3, itembtn_4)
-	markup.row(itembtn_5, itembtn_6)
-	bot.send_message(chat_id, "выбери категорию:", reply_markup=markup)
+def test():
+	print('функция сработала')
+
+
+@bot.message_handler(commands=['s'])
+def main_menu(message):
+	keyboard = types.InlineKeyboardMarkup()
+	keyboard_1 = types.InlineKeyboardButton(text='продукты', callback_data='test')
+	keyboard.add(keyboard_1)
+	keyboard_2 = types.InlineKeyboardButton(text='тачка', callback_data='test')
+	keyboard.add(keyboard_2)
+	keyboard_3 = types.InlineKeyboardButton(text='пиво', callback_data='beer')
+	keyboard.add(keyboard_3)
+	bot.send_message(message.from_user.id, text='Выбери категорию', reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+	if call.data == "test":
+		bot.send_message(call.message.chat.id, 'введи сумму:')
+	if call.data == "beer":
+		bot.send_message(call.message.chat.id, 'а вот это отлично')
+
+
+@bot.message_handler()
+def answer(message):
+	bot.send_message(message.from_user.id, text=f'сумма = {message.text}')
 
 
 bot.infinity_polling()
